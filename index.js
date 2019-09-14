e1b346aaf0b494e82e5ab0ca2f228e2234751b7b
Fannar Snær <fannar@lambagras.is>
2019-09-14 13:40:58 +0000

    Formatting

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ SlackAPI.prototype.post = async function post (path, body, token) {
   return this.callAPI(path, {
     method: 'POST',
     headers: {
-      'authorization': `Bearer ${token}`,
+      authorization: `Bearer ${token}`,
       'content-type': 'application/json;charset=utf-8'
     },
     body
@@ -55,7 +55,11 @@ SlackAPI.prototype.get = async function get (path, params, token) {
   })
 }
 
-SlackAPI.prototype.getPaginated = async function getPaginated (path, params, token) {
+SlackAPI.prototype.getPaginated = async function getPaginated (
+  path,
+  params,
+  token
+) {
   const [err, result] = await this.get(path, params, token)
 
   if (err) {
@@ -63,7 +67,11 @@ SlackAPI.prototype.getPaginated = async function getPaginated (path, params, tok
   }
 
   if (result.response_metadata && result.response_metadata.next_cursor) {
-    const [nextErr, nextPage] = await this.getPaginated(path, {...params, cursor: result.response_metadata.next_cursor}, token)
+    const [nextErr, nextPage] = await this.getPaginated(
+      path,
+      { ...params, cursor: result.response_metadata.next_cursor },
+      token
+    )
 
     if (nextErr) {
       return [nextErr, undefined]
@@ -84,9 +92,13 @@ SlackAPI.prototype.getMembers = async function getMembers () {
 
 SlackAPI.prototype.filePublicUrl = async function filePublicUrl (fileId) {
   // Note: this method requires a user access token.
-  return this.post('files.sharedPublicURL', {
-    file: fileId
-  }, this.userAccessToken)
+  return this.post(
+    'files.sharedPublicURL',
+    {
+      file: fileId
+    },
+    this.userAccessToken
+  )
 }
 
 SlackAPI.prototype.userInfo = async function userInfo (userId) {
@@ -95,14 +107,22 @@ SlackAPI.prototype.userInfo = async function userInfo (userId) {
   })
 }
 
-SlackAPI.prototype.getPermalink = async function getPermalink (channelId, messageId) {
+SlackAPI.prototype.getPermalink = async function getPermalink (
+  channelId,
+  messageId
+) {
   return this.get('chat.getPermalink', {
     channel: channelId,
     message_ts: messageId
   })
 }
 
-SlackAPI.prototype.postMessage = async function postMessage (channel, text, mrkdwn, attachments) {
+SlackAPI.prototype.postMessage = async function postMessage (
+  channel,
+  text,
+  mrkdwn,
+  attachments
+) {
   return this.post('chat.postMessage', {
     channel: channel,
     text: text,
